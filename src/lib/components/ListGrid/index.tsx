37131c107786dc64fb2a749583eb0a89a6a30137
Ivan <45449502+vatergo@users.noreply.github.com>
2024-02-13 02:46:31 +0300

    Проапгрейдить чекбосовый фильтр (#5)
    * feat: add header for ListGrid
    
    * fix: minor
    
    * fix: font-size
    
    * fix: font-sizes
    
    * fix: styles
    
    * fix: styles
    
    * publish tag
    
    ---------
    
    Co-authored-by: Anton Akhatov <anton@gcor.ru>

--- src/lib/components/ListGrid/index.tsx
+++ src/lib/components/ListGrid/index.tsx
@@ -2,17 +2,18 @@ import { ReactNode } from 'react';
 import React from 'react';
 import styled, { css } from 'styled-components';
 import { getBackgroundStyle } from '../../utils/getBackgroundStyle';
+import { Divider } from '../Divider';
 
 const Wrapper = styled.label`
   display: flex;
   align-items: flex-start;
-  font-size: 12px;
+  font-size: 14px;
   line-height: 18px;
   user-select: none;
   cursor: pointer;
   ${({ theme }) => css`
     gap: ${theme.size.m};
-    padding: ${theme.size.m} ${theme.size.m};
+    padding: ${theme.size.s} ${theme.size.m};
     border-radius: ${theme.size.s};
     &:hover {
       ${getBackgroundStyle({
@@ -27,6 +28,12 @@ const Wrapper = styled.label`
       })};
     }
   `}
+
+  @supports (grid-template-columns: subgrid) {
+    display: grid;
+    grid-template-columns: subgrid;
+    grid-column: span 3;
+  }
 `;
 
 const Content = styled.div`
@@ -45,7 +52,6 @@ const TitleWrapper = styled.div`
 `;
 
 const Title = styled.span`
-  font-size: 14px;
   font-weight: 500;
   white-space: nowrap;
   text-overflow: ellipsis;
@@ -54,13 +60,13 @@ const Title = styled.span`
 
 const SubTitle = styled.span`
   ${({ theme }) => css`
-    color: ${theme.text.color.primary};
+    color: ${theme.text.color.tertiary};
   `}
-  margin-top: 1px;
   font-feature-settings: 'tnum';
 `;
 
 const Description = styled.div`
+  font-size: 12px;
   ${({ theme }) => css`
     color: ${theme.text.color.tertiary};
   `}
@@ -70,11 +76,11 @@ const Prefix = styled.div``;
 
 const Postfix = styled.div`
   ${({ theme }) => css`
-    color: ${theme.text.color.tertiary};
+    color: ${theme.text.color.primary};
   `}
   white-space: nowrap;
-  margin-top: 1px;
   font-feature-settings: 'tnum';
+  text-align: end;
 `;
 
 interface Props {
@@ -92,6 +98,11 @@ export interface ListGridProps {
 export const ListGrid = styled.div<ListGridProps>`
   display: flex;
   flex-direction: column;
+
+  @supports (grid-template-columns: subgrid) {
+    display: grid;
+    grid-template-columns: max-content auto max-content;
+  }
 `;
 
 export function ListGridItem({
@@ -113,5 +124,63 @@ export function ListGridItem({
       </Content>
       {postfix && <Postfix>{postfix}</Postfix>}
     </Wrapper>
+  );
+}
+
+const HeaderContent = styled(Content)`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const HeaderTitleWrapper = styled(TitleWrapper)`
+  justify-content: normal;
+  align-items: baseline;
+`;
+
+const HeaderDescription = styled(Description)`
+  font-size: 14px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+const HeaderDividerWrapper = styled.div`
+  ${({ theme }) => css`
+    margin-right: ${theme.size.m};
+    margin-left: ${theme.size.m};
+  `}
+
+  @supports (grid-template-columns: subgrid) {
+    grid-column: span 3;
+  }
+`;
+
+export function ListGridHeader({
+  subTitle,
+  description,
+  prefix,
+  postfix,
+  children,
+}: Props) {
+  return (
+    <>
+      <Wrapper>
+        {prefix && <Prefix>{prefix}</Prefix>}
+        <HeaderContent>
+          <HeaderTitleWrapper>
+            {children && <Title>{children}</Title>}
+            {description && (
+              <HeaderDescription>{description}</HeaderDescription>
+            )}
+          </HeaderTitleWrapper>
+          {subTitle && <SubTitle>{subTitle}</SubTitle>}
+        </HeaderContent>
+        {postfix && <Postfix>{postfix}</Postfix>}
+      </Wrapper>
+
+      <HeaderDividerWrapper>
+        <Divider verticalGap={4} />
+      </HeaderDividerWrapper>
+    </>
   );
 }
